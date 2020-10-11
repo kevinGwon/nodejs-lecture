7350a8a2d604fca834843e60a5faa091f3175522
kevinGwon <developerGwon@gmail.com>
2020-10-11 22:33:51 +0900

    Update Week-5

--- week-5/week-5-2-2.js
+++ week-5/week-5-2-2.js
@@ -3,12 +3,23 @@ const http = require('http');
 
 http
   .createServer((req, res) => {
-    fs.readFile('./assets/index.html', (err, file) => {
-      res.writeHead(200, {
-        'Content-type': 'text/html',
-      });
-      res.end(file);
+    // HTML
+    // fs.readFile('./assets/index.html', (err, file) => {
+    //   res.writeHead(200, {
+    //     'Content-type': 'text/html',
+    //   });
+    //   res.end(file);
+    // });
+
+    // Cookie
+    const date = new Date();
+    date.setDate(date.getDate() + 7);
+
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+      'Set-Cookie': [`breakfast=toast;Expires=${date.toUTCString()}`],
     });
+    res.end('<p>Set Cookie</p>');
   })
   .listen(3000, () => {
     console.log('Server running : http://127.0.0.1:3000/');
